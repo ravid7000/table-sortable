ed9c78026b3b193f94ea55fae1bf9b199ead4376
Ravi Dhiman <ravid7000@gmail.com>
2020-02-10 17:56:51 +0530

    publish command added

---
+++ publish.js
@@ -0,0 +1,18 @@
+const ghpages = require('gh-pages')
+const pkg = require('./package.json')
+
+const branchName = ver => `release/${ver}`
+const tag = ver => `v${ver}`
+const message = (ver, brnch) => `Release branch ${brnch} to latest version ${ver}`
+const branch = branchName(pkg.version)
+
+const options = {
+    branch,
+    tag: tag(pkg.version),
+    message: message(pkg.version, branch),
+}
+
+ghpages.publish('dist', options, function() {
+    console.log(options)
+    console.log('Done!!!')
+})
