6d45c61fd1fc0a9c5ac7b05e163df03ea7ced5ef
Ravi Dhiman <ravid7000@gmail.com>
2022-01-07 22:15:15 +0530

    removed unused files

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import devConfig from './configs/rollup.config.dev'
-import prodConfig from './configs/rollup.config.prod'
-
-const production = !process.env.ROLLUP_WATCH
-
-const config = production ? prodConfig : devConfig
-
-export default config
